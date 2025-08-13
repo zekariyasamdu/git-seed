85205375e75e928a30bfb03cf32f6ddc76dfc24f
zach <zekariyasamdu24@gmail.com>
2025-08-13 15:43:05 -0500

    feat: setting up SSL mutual check

--- system/socket_network/socket_client.py
+++ system/socket_network/socket_client.py
@@ -1,19 +1,29 @@
 import socket
+import ssl
 
 HOST = "127.0.0.1"
 PORT = 6000
 CID = "QmWfVY9y3xjsixTgbd9AorQxH7VtMpzfx2HaWtsoUYecaX"
 out_file = "downloaded_from_ipfs"
 
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    s.connect((HOST, PORT))
-    s.sendall(CID.encode())  # Send the CID to server
+# SSL context for proper verification
+context = ssl.create_default_context(cafile="./test_keys/ca_cert.pem")  # CA cert that signed server certificate
+context.check_hostname = True  # Verify server hostname matches certificate
 
-    with open(out_file, "wb") as f:
-        while True:
-            data = s.recv(1024)
-            if not data:
-                break
-            f.write(data)
+# for mutual TLS (if server requires client certificate)
+context.load_cert_chain(certfile="./test_keys/client_cert.pem", keyfile="./test_keys/client_key.pem")
 
-print("File downloaded!")
+with socket.create_connection((HOST, PORT)) as sock:
+    with context.wrap_socket(sock, server_hostname=HOST) as ssock:
+        # Send CID
+        ssock.sendall(CID.encode())
+
+        # Receive file
+        with open(out_file, "wb") as f:
+            while True:
+                data = ssock.recv(8192)
+                if not data:
+                    break
+                f.write(data)
+
+print("File downloaded with verified SSL!")
